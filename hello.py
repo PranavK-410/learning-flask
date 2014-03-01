12671aad574663f7779881f46e18b3cfafdf5f71
haya14busa <hayabusa1419@gmail.com>
2014-03-01 09:50:37 +0900

    Add comment on hello.py

--- hello.py
+++ hello.py
@@ -25,10 +25,22 @@
 #
 #=============================================================================
 
+''' Import the Flask class.
+An Instance of this class will be our WSGI application
+'''
 from flask import Flask
+
+''' Create an instance of this class.
+The first argument is the name of the application's module or package.
+Single module -> `__name__`
+This is needed so that Flask knows where to look for templates, static files,
+and so on
+'''
 app = Flask(__name__)
 
 
+# Use the `route()` decorator
+# to tell Flask what URL should trigger our function
 @app.route('/')
 def hello():
     return 'Happy Vimming!'
