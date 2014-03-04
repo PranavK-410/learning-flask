fe4e2a16d0585d118364ef8fff9aa1003bdb18c3
haya14busa <hayabusa1419@gmail.com>
2014-03-04 13:48:16 +0900

    Test nltk path

--- hello.py
+++ hello.py
@@ -26,6 +26,7 @@
 #=============================================================================
 
 from random import choice
+import nltk
 from nltk.corpus import stopwords
 
 ''' Import the Flask class.
@@ -153,7 +154,8 @@ def send_title():
 
 @app.route('/nltk/stopwords')
 def show_stopwords():
-    return str(stopwords.words('english'))
+    return str(nltk.data.path)
+    # return str(stopwords.words('english'))
 
 
 def main():
